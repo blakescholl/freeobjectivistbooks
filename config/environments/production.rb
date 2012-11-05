8a533acd983ec39bfee61230f622394283184410
Jason Crawford <jason@jasoncrawford.org>
2012-11-04 18:13:39 -0800

    Put back the precompile the way it was
    This was a hack that doesn't seem to be working.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ FreeBooks::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += [/^[a-z0-9]\w+.(css|js)$/]
+  config.assets.precompile += %w( *.js *.css )
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true

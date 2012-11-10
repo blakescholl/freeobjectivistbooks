dc189e0ca83d07baa2479e6bdcd3c0f2f792583b
Jason Crawford <jcrawford@groupon.com>
2012-11-09 23:01:14 -0500

    Add blocked bit to ActiveAdmin and customize the user admin a bit

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -4,6 +4,7 @@ ActiveAdmin.register User do
   filter :studying
   filter :school
   filter :location
+  filter :blocked
 
   index do
     selectable_column
@@ -13,6 +14,36 @@ ActiveAdmin.register User do
     column :studying
     column :school
     column :location
+    column :blocked
     default_actions
+  end
+
+  show do |user|
+    attributes_table do
+      row :id
+      row :name
+      row :email
+      row :studying
+      row :school
+      row :location
+      row :address
+      row :blocked
+      row :created_at
+      row :updated_at
+    end
+    active_admin_comments
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :email
+      f.input :studying
+      f.input :school
+      f.input :location
+      f.input :address
+      f.input :blocked
+    end
+    f.buttons
   end
 end

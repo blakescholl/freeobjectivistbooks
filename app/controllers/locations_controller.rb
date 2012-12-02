05dd0ee11f899a9c2c7e733072e4303e7463c83d
Jason Crawford <jason@jasoncrawford.org>
2012-12-01 18:55:12 -0800

    Fix for crasher on /locations page
    If a nil value gets into the array with a bunch of strings, sort will fail with an ArgumentError ("comparison of NilClass with String failed").

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -4,7 +4,7 @@ class LocationsController < ApplicationController
     @users = Request.active.includes(:user).all.map {|request| request.user}.uniq
     @location_names = @users.map {|user| user.location}.uniq
     @locations = Location.where(name: @location_names)
-    @countries = @locations.map {|location| location.country}.uniq.sort
+    @countries = @locations.map {|location| location.country}.uniq.compact.sort
     @markers = @locations.select {|location| location.locality?}
   end
 end

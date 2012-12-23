52765057c80624a6f95002ad346051e3bacf5d05
Jason Crawford <jcrawford@groupon.com>
2012-12-23 02:52:03 -0500

    Better workaround for ActiveAdmin bug

--- app/models/balance_observer.rb
+++ app/models/balance_observer.rb
@@ -1,16 +1,23 @@
 class BalanceObserver < ActiveRecord::Observer
   observe :contribution, :donation
 
+  #--
+  # NOTE: We switch on class names here rather than classes directly because of a problem with ActiveAdmin and class
+  # caching. When classes are not cached in development mode, ActiveAdmin is still holding on to the original classes
+  # and these case statements don't behave the way you'd expect. Hence switching on strings instead of the classes
+  # themselves. For a bit of context, see: https://github.com/gregbell/active_admin/issues/835
+  #++
+
   def after_create(object)
-    case object
-    when Contribution
+    case object.class.name
+    when "Contribution"
       contribution = object
       contribution.add_to_user_balance
       donations = contribution.user.donations.needs_payment.reorder(:created_at)
       donations.each do |donation|
         donation.pay_if_covered
       end
-    when Donation
+    when "Donation"
       object.pay_if_covered
     end
   end
@@ -25,10 +32,10 @@ class BalanceObserver < ActiveRecord::Observer
 
   def after_destroy(object)
     user = object.user
-    case object
-    when Contribution
+    case object.class.name
+    when "Contribution"
       object.subtract_from_user_balance
-    when Donation # should never happen
+    when "Donation" # should never happen
       object.unpay
     end
   end

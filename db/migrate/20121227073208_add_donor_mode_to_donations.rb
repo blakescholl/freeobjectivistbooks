2a20118273b4cd9cef34b7594f2eac5752650452
Jason Crawford <jcrawford@groupon.com>
2012-12-27 12:27:29 -0500

    Fix migration SQL for Postgres

--- db/migrate/20121227073208_add_donor_mode_to_donations.rb
+++ db/migrate/20121227073208_add_donor_mode_to_donations.rb
@@ -1,7 +1,7 @@
 class AddDonorModeToDonations < ActiveRecord::Migration
   def up
     add_column :donations, :donor_mode, :string, null: false, default: "send_books"
-    execute 'update donations d join users u on user_id=u.id set d.donor_mode = u.donor_mode where price_cents is not null'
+    execute 'update donations d set d.donor_mode = u.donor_mode from users u where user_id=u.id and price_cents is not null'
   end
 
   def down

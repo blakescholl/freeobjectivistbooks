e97605fb2f59a7a1dfe35c9e53c0ddaf938c23d4
Jason Crawford <jcrawford@groupon.com>
2012-12-29 18:40:59 -0500

    Fix bug that was showing raw HTML on admin pages

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -6,6 +6,6 @@ module AdminHelper
   def admin_user_links(users)
     users = Array(users).flatten.compact
     links = users.map {|user| admin_user_link user}
-    links.to_sentence
+    raw links.to_sentence
   end
 end

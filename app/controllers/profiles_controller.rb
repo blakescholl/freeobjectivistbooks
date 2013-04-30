8fe64d4f8396d88a69632d06345dd41df99dc473
Jason Crawford <jason@jasoncrawford.org>
2013-04-30 12:36:41 -0700

    Minor simplification in profile controller
    Refer to @current_user just as @user. Makes some standard stuff easier
    in the forthcoming edit/update actions

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,20 +2,24 @@
 class ProfilesController < ApplicationController
   before_filter :require_login
 
-  def show
-    @requests = @current_user.requests
+  def load_models
+    @user = @current_user
+  end
 
-    @pledge = @current_user.latest_pledge
+  def show
+    @requests = @user.requests
+
+    @pledge = @user.latest_pledge
     @pledge_donations_count = @pledge.donations.size if @pledge
 
-    donations = @current_user.donations.active
-    @show_donations = donations.any? || @current_user.pledges.any?
+    donations = @user.donations.active
+    @show_donations = donations.any? || @user.pledges.any?
 
     @outstanding_donations = donations.needs_donor_action
     @flag_count = donations.unpaid.not_sent.flagged.count
     @any_eligible = @outstanding_donations.any? {|donation| donation.can_send_money?}
 
-    @fulfillments = @current_user.fulfillments.needs_sending
-    @show_fulfillments = @fulfillments.any? || @current_user.is_volunteer?
+    @fulfillments = @user.fulfillments.needs_sending
+    @show_fulfillments = @fulfillments.any? || @user.is_volunteer?
   end
 end
